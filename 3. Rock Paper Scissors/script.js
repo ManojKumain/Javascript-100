1bba74febe6564900fdf3cca17d5e14abe663084
Manoj Chand <memanojkumain@gmail.com>
2025-10-11 07:51:53 +0530

    added logic to play a single round

--- 3. Rock Paper Scissors/script.js
+++ 3. Rock Paper Scissors/script.js
@@ -17,7 +17,7 @@ function getComputerChoice() {
     const randomChoice = Math.floor(Math.random() * 3 + 1);
 
     // 3. return choice that maches the randomChoice
-    if ( randomChoice === 1) {
+    if (randomChoice === 1) {
         return choice1
     } else if (randomChoice === 2) {
         return choice2
@@ -27,12 +27,57 @@ function getComputerChoice() {
 
 }
 
+
 // Step 3: Write the logic to get the human choice
 
 function getHumanChoice() {
     const humanChoice = prompt("Enter Your Choice").toLowerCase()
     return humanChoice;
 }
+
+
+// Step 4: Declare the players score variables
+let humanScore = 0;
+let computerScore = 0;
+
+
+// Step 5: Write the logic to play a single round
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie! No one wins");
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log(`You lose! Paper beats rock`);
+        computerScore++;
+    }
+    else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log(`You win! rock beats scissors`);
+        humanScore++;
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++
+    }
+    else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerChoice++;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerChoice++;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++
+    }
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection)
 
 
 
